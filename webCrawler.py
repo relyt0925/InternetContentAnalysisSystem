6d3016ac114a2a85c06fbfb51cdd72b3ff398f56
Tyler Lisowski <tlisowsk@cisco.com>
2015-09-06 18:23:14 -0400

    V1.1 Threaded Web Crawler

--- webCrawler.py
+++ webCrawler.py
@@ -5,22 +5,23 @@ Created on Sep 5, 2015
 '''
 from lxml import html
 import requests
-import sys
 import os
 import csv
-import codecs
+import threading
+from datetime import datetime
 
 class WebContentCrawler(object):
     '''
     Web Crawler for web page content analysis system
     '''
-
+    
 
     def __init__(self, webPageListPath,):
         '''
         Constructor
         '''
         self.webpageListPath=webPageListPath;
+        self.threadMax=10;
         return;
     
     def fetchDomTree(self,url):
@@ -29,11 +30,15 @@ class WebContentCrawler(object):
         tree= html.fromstring(page.content);
         return {url : tree}
     
+    def fetchAndUpdateDict(self,url,urlTreeDict):
+        tempDict= self.fetchDomTree(url);
+        urlTreeDict.update(tempDict);
+        return;
+    
     def storeTree(self,urlDomTreeDictionary,timePath):
         #add in iterator for list
         if not os.path.exists(timePath):
             os.makedirs(timePath)
-            
         for key in urlDomTreeDictionary:
             f= open(timePath+key+'.html','a');
             f.write(html.tostring(urlDomTreeDictionary[key]));
@@ -45,18 +50,34 @@ class WebContentCrawler(object):
         tree=html.fromstring(f.read());
         return tree;
     
-    def fetchTopPages(self,numPages):
+    def fetchAndStoreTopPages(self,numPages):
+        time=datetime.now()
+        timeString=str(time.year)+'_'+str(time.month)+'_'+str(time.day)+'_'+str(time.hour)+'/';
         f= open(self.webpageListPath,'r');
         csvReader=csv.reader(f,delimiter=',');
         tempNumPages= numPages;
         urlTreeDict={};
+        threadList=[];
         for row in csvReader:
-            tempDict= self.fetchDomTree(row[1]);
-            urlTreeDict.update(tempDict);
-            tempNumPages-=1;
             if tempNumPages <= 0:
                 break;
-        return urlTreeDict;
+            tempNumPages-=1;
+            t= threading.Thread(target=self.fetchAndUpdateDict, args=(row[1],urlTreeDict));
+            t.daemon=False;
+            t.start();
+            threadList.append(t);
+            if len(threadList)>=self.threadMax:
+                for i in threadList:
+                    i.join();
+                del threadList[:];
+            self.storeTree(urlTreeDict, 'web/'+timeString);
+            urlTreeDict.clear();
+            
+        for i in threadList:
+            i.join();
+        self.storeTree(urlTreeDict, 'web/'+timeString);
+        return;
+    
 
     
     
--- webCrawlerDriver.py
+++ webCrawlerDriver.py
@@ -4,22 +4,17 @@ Created on Sep 6, 2015
 @author: tlisowsk
 '''
 from webCrawler import WebContentCrawler
-import os
 from datetime import datetime
 from lxml import html
 
 def webCrawlerDriver():
     webCrawler=WebContentCrawler('top-1m.csv');
-    pages=webCrawler.fetchTopPages(3);
+    webCrawler.fetchAndStoreTopPages(30);
     time=datetime.now()
     timeString=str(time.year)+'_'+str(time.month)+'_'+str(time.day)+'_'+str(time.hour)+'/';
-    webCrawler.storeTree(pages, 'web/'+timeString);
+    #webCrawler.storeTree(pages, 'web/'+timeString);
     cmpTree=webCrawler.loadTreeFromMem('web/'+timeString+'google.com.html');
-    if html.tostring(cmpTree) == html.tostring(pages['google.com']):
-        print str(True);
-    for key in pages:
-        print key;
-        #print pages[key];
+    print html.tostring(cmpTree);
     return;
             
         
